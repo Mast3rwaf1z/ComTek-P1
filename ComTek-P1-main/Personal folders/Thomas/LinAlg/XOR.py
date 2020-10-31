c73ce176679f0f8386c45756ad09d0300230090f
Mast3rwaf1z <32744171+Mast3rwaf1z@users.noreply.github.com>
2020-10-31 17:24:43 +0100

    Delete XOR.py

--- ComTek-P1-main/Personal folders/Thomas/LinAlg/XOR.py
+++
@@ -1,9 +0,0 @@
-def XOR(p1, p2):
-    A = [p1, p2]
-    result = []
-    for i in range(len(p1)):
-        if A[0][i] == A[1][i]:
-            result.append(0)
-        elif A[0][i] != A[1][i]:
-            result.append(1)
-    return result
