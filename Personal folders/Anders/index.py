8396e0507ab52caac2903de3eac6d1b620ddcee4
holmdane <57073689+holmdane@users.noreply.github.com>
2020-11-13 10:20:27 +0100

    my update

---
+++ Personal folders/Anders/index.py
@@ -0,0 +1,12 @@
+import requests
+import json
+from datetime import datetime
+
+data = {
+    "email": "sjuhl20@student.aau.dk",
+    "password": "wWnxiryiCBtU3Ta"
+}
+
+x = json.dumps(data)
+
+print(x)
