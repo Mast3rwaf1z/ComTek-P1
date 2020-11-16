7be82e8228da156e3b42606ab524ed8c942f262a
Mast3rwaf1z <32744171+Mast3rwaf1z@users.noreply.github.com>
2020-11-16 08:32:52 +0100

    Add files via upload

--- Personal folders/Thomas/chirpstack/getEUI.py
+++ Personal folders/Thomas/chirpstack/getEUI.py
@@ -3,8 +3,8 @@ import binascii
 lora = LoRa(mode=LoRa.LORAWAN,region=LoRa.EU868)
 print(binascii.hexlify(lora.mac()))
 
-app_eui = binascii.unhexlify("0101010101010101")
-app_key = "c4 6d 07 1a d4 9b 09 ce 5d 77 6f 29 81 34 9b a0"
+app_eui = binascii.unhexlify("0000000000000000")
+app_key = binascii.unhexlify("01020304050607080910111213141516")
 
 print("DevEUI: %s" % binascii.hexlify(lora.mac()))
 print("AppEUI: %s" % binascii.hexlify(app_eui))
